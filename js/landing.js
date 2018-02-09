8bd47183ed422ea24791a04211e03871acb0f047
Miralem Drek <mek@qlik.com>
2018-02-09 15:45:01 +0100

    Deploy website
    Deploy website version based on HEAD

---
+++ js/landing.js
@@ -0,0 +1,31 @@
+(function() {
+  var scrollPosY = 0;
+  document.addEventListener('DOMContentLoaded', function() {
+    if (!document.querySelector('#threed-chart')) {
+      return;
+    }
+
+    function transform() {
+      var s = ['components'];
+      s.push('state-' + (Math.min(2, Math.floor(scrollPosY/120))));
+      document.querySelector('#threed-chart').className = s.join(' ');
+    }
+
+    var timer = null;
+
+    window.addEventListener('scroll', function(e) {
+      if (scrollPosY === window.scrollY) {
+        return;
+      }
+
+      scrollPosY = window.scrollY;
+
+      if (!timer) {
+        timer = window.requestAnimationFrame(function () {
+          transform();
+          timer = null;
+        });
+      }
+    });
+  });
+}());
