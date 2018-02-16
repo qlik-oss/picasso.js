d9af05afbdeb217ce333f29d2c12166168901a85
Hannes Lindquist <hannes.lindquist@qlik.com>
2018-02-16 14:48:03 +0100

    footer links

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -31,16 +31,16 @@ class Footer extends React.Component {
             </a>
           </div>
           <div>
-            <h5>&nbsp;</h5>
-            <div className="footerIcons">
-              <a href="https://github.com/qlik-oss/picasso.js">
-                <i className="fa fa-github" aria-hidden="true"></i>
-              </a>
-              <a href="http://qlikbranch-slack-invite.herokuapp.com/">
-                <i className="fa fa-slack" aria-hidden="true"></i>
-              </a>
-              <a className="github-button" href="https://github.com/qlik-oss/picasso.js" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star qlik-oss/picasso.js on GitHub">Star</a>
-            </div>
+            <h5>Links</h5>
+            <a href="http://qlikbranch-slack-invite.herokuapp.com/">
+              <i className="fa fa-slack"></i> Slack
+            </a>
+            <a href="https://twitter.com/picassodotjs">
+              <i className="fa fa-twitter"></i> Twitter
+            </a>
+            <a href="https://github.com/qlik-oss/picasso.js">
+              <i className="fa fa-github"></i> GitHub
+            </a>
           </div>
         </section>
 
