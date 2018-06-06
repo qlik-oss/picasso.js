f1cf118ba23134a5e285f4d6102d98b30d0c3961
Hannes Lindquist <hannes.lindquist@qlik.com>
2018-06-06 16:41:40 +0200

    fix(picasso-q): Register number & time formatter when using picasso-plugin-q

--- plugins/q/src/index.js
+++ plugins/q/src/index.js
@@ -1,10 +1,14 @@
 import data from './data/dataset';
 import qBrushHelper from './brush/q-brush';
+import numberFormat from './formatter/numberFormat';
+import timeFormat from './formatter/timeFormat';
 // import qTime from './scales/qTime';
 
 export default function initialize(picasso) {
   data.util = picasso.data('matrix').util;
   picasso.data('q', data);
+  picasso.formatter('q-number', numberFormat);
+  picasso.formatter('q-time', timeFormat);
   // picasso.scale('qTime', qTime);
 }
 
