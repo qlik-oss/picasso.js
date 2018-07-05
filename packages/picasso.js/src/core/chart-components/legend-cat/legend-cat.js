1cb06f40145bb217123e9748445c54b9e3c72570
Christofer Bengtsson <cbt@qlik.com>
2018-07-05 09:40:02 +0200

    first draft

--- packages/picasso.js/src/core/chart-components/legend-cat/legend-cat.js
+++ packages/picasso.js/src/core/chart-components/legend-cat/legend-cat.js
@@ -170,7 +170,7 @@ const component = {
     this.navigationRenderer = navigationRendererFactory(this);
     this.titleRenderer = titleRendererFactory(this);
     this.navigationRenderer.renderer = this.registries.renderer('dom')();
-    this.titleRenderer.renderer = this.registries.renderer()();
+    this.titleRenderer.renderer = this.registries.renderer('svg')();
     update(this);
   },
   preferredSize(obj) {
