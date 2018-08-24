7076b113516c33b50b8eff529801622e88359894
Anders Nilsson <nilssonanders79@gmail.com>
2018-08-24 15:48:27 +0200

    jsx in tooltip

---
+++ packages/picasso.js/src/core/utils/class-string.js
@@ -0,0 +1,8 @@
+/**
+ * Concatenate object keys into a space separated string. Use for transforming a 'class name map' into a class string
+ * @param  {Object} classMap Object with class names as keys and true or false values depending on if they should be in the returned class string or not
+ * @return {String} Space separated string with class names
+ */
+export default function classString(classMap) {
+  return Object.keys(classMap).filter(className => classMap[className]).join(' ');
+}
