717d0fafc8afba4a80539468fbdc0d32105fd419
Anders Nilsson <nilssonanders79@gmail.com>
2018-09-11 11:57:10 +0200

    refactor: remove chart-mixins and component-mixins

--- packages/picasso.js/src/core/component/index.js
+++ packages/picasso.js/src/core/component/index.js
@@ -1,22 +1,4 @@
-// import * as mixins from './component-mixins';
 import registry from '../utils/registry';
 
 const componentRegistry = registry();
-
-// TODO - mixins?
-
 export { componentRegistry as default };
-
-// export default function componentFactory(parentRegistry) {
-//   const reg = registry(parentRegistry);
-
-//   function component(name, definition) {
-//     if (definition) {
-//       reg.register(name, definition);
-//     }
-//     return reg.get(name);
-//   }
-//   component.mixin = mixins.add;
-
-//   return component;
-// }
