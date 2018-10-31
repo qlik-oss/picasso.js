6306fc61610aa22fd58bdf5632a4582943278777
Pontus Nyman <pna@qlik.com>
2018-10-31 10:59:36 +0100

    test: interactions with picasso

--- test/puppet/barchart/barchart.puppet.js
+++ test/puppet/barchart/barchart.puppet.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 
 describe('picasso-barchart', () => {
-  const fixture = 'http://localhost:10001/barchart/barchart.fix.html';
+  const fixture = 'http://localhost:10001/barchart/barchart-lasso.fix.html';
+  const fixtureRange = 'http://localhost:10001/barchart/barchart-range.fix.html';
 
   it('Select some bars', async () => {
     await page.goto(fixture);
@@ -13,8 +14,56 @@ describe('picasso-barchart', () => {
     expect(aprisSelected).to.equal(true);
     expect(marisSelected).to.equal(true);
   });
-  it.skip('Range select bars', async () => {
+
+  it('lasso select in barchart', async () => {
     await page.goto(fixture);
     await page.waitForSelector('.container');
+    await page.mouse.move(80,100);
+    await page.mouse.down();
+    await page.mouse.move(80,400);
+    await page.mouse.move(280,400);
+    await page.mouse.move(280,100);
+    await page.mouse.move(80,100);
+    await page.mouse.up();
+    let febisSelected = await page.evaluate(() => picassochart.brush('highlight').containsValue('0/Month', 'Feb'));
+    expect(febisSelected).to.equal(true, 'Expected feb to be selected');
+  });
+
+  // Due to a bug the barchart aint brushing.
+  it.skip('Range select y-axis bar in barchart', async () => {
+    await page.goto(fixtureRange);
+    await page.waitForSelector('.container');
+    await page.mouse.move(10,10);
+    await page.mouse.down();
+    await page.mouse.move(10,200);
+    await page.mouse.up();
+    let rangeSelect = await page.evaluate(() => picassochart.brush("highlight").containsRange('0/Sales', { min: 9000, max: 9100 }));
+    console.log(rangeSelect);
+  });
+
+  // Due to a bug the barchart aint brushing.
+  it.skip('Range select x-axis bar in barchart', async () => {
+    await page.goto(fixtureRange);
+    await page.waitForSelector('.container');
+    await page.mouse.move(80,800);
+    await page.mouse.down();
+    await page.mouse.move(350,800);
+    await page.mouse.up();
+    let janisSelected = await page.evaluate(() => picassochart.brush('highlight').containsValue('0/Month', 'Jan'));
+    let febisSelected = await page.evaluate(() => picassochart.brush('highlight').containsValue('0/Month', 'Apr'));
+    expect(janisSelected).to.equal(true);
+    expect(febisSelected).to.equal(true);
+  });
+  // Due to a bug the barchart aint brushing.
+  it.skip('Move range-select', async () => {
+    await page.goto(fixtureRange);
+    await page.waitForSelector('.container');
+    await page.evaluate(() => picassochart.brush("highlight").setRange('0/Sales', { min: 4000, max: 7000 }));
+  });
+  // Due to a bug the barchart aint brushing.
+  it.skip('Resize range-select', async () => {
+    await page.goto(fixtureRange);
+    await page.waitForSelector('.container');
+    await page.evaluate(() => picassochart.brush("highlight").setRange('0/Sales', { min: 4000, max: 7000 }));
   });
 });
--- test/puppet/hammer/hammer.puppet.js
+++ test/puppet/hammer/hammer.puppet.js
@@ -7,15 +7,15 @@ describe('picasso-hammer', () => {
   const fixture = 'http://localhost:10001/hammer/hammer.fix.html';
   // const click = new Actions();
   // const clickAt = 1;
-  it('should trigger tap event on chart', async () => {
+  it.skip('should trigger tap event on chart', async () => {
     console.log(fixture);
     await page.goto(fixture);
 
-    const getTapCount = () => window.tapCount;
+    // const getTapCount = () => window.tapCount;
 
-    const element = await page.('#container');
-    await clickAt(element, { x: 10, y: 10 });
-    const tapCount = await browser.executeScript(getTapCount);
-    expect(tapCount).to.equal(1);
+    const element = await page.waitForSelector('#container');
+    // await clickAt(element, { x: 10, y: 10 });
+    // const tapCount = await browser.executeScript(getTapCount);
+    // expect(tapCount).to.equal(1);
   });
 });
