2367b753bea1f787c45c42753ce69dcc3a774a78
HVA <quan.ho@qlik.com>
2019-04-03 09:36:39 +0200

    fix(bar-label): fix calculation of bound rotation

--- packages/picasso.js/src/core/chart-components/labels/strategies/bars.js
+++ packages/picasso.js/src/core/chart-components/labels/strategies/bars.js
@@ -2,12 +2,6 @@ import extend from 'extend';
 import {
   testRectRect
 } from '../../../math/narrow-phase-collision';
-import { rotate } from '../../../math/vector';
-import {
-  rectToPoints,
-  pointsToRect
-} from '../../../geometry/util';
-import { toRadians } from '../../../math/angles';
 import filterOverlapping from './bars-overlapping-filter';
 
 const LINE_HEIGHT = 1.5;
@@ -189,33 +183,20 @@ export function findBestPlacement({
 }
 
 function approxTextBounds(label, textMetrics, rotated, rect) {
-  const x = label.x + label.dx;
-  const y = label.y + label.dy;
-  const width = rotated ? textMetrics.width : Math.min(textMetrics.width, rect.width);
-  const height = rotated ? Math.min(textMetrics.height, rect.height) : textMetrics.height;
-  const PADDING_OFFSET = 1e-6; // Needed to support a case when multiple bars are on the same location
-  const delta = 0.75; // the difference between text-before-edge and alphabetical
-
-  let bounds = {
+  const x0 = label.x + label.dx;
+  const y0 = label.y + label.dy;
+  const height = rotated ? Math.min(textMetrics.width, rect.height) : Math.min(textMetrics.height, rect.width);
+  const width = rotated ? Math.min(textMetrics.height, rect.height) : Math.min(textMetrics.width, rect.width);
+  const offset = 0.8 * textMetrics.height; // the distance between text-before-edge and alphabetical
+  const PADDING_OFFSET = 1e-9; // Needed to support a case when multiple bars are on the same location
+  const x = rotated ? x0 - offset : x0;
+  const y = rotated ? y0 : y0 - offset;
+  const bounds = {
     x: x - PADDING - PADDING_OFFSET,
-    y: y - height * delta - PADDING - PADDING_OFFSET, // bounds is based on text-before-edge so it needs to shift up 75% to match the label is based on alphabetical
+    y: y - PADDING - PADDING_OFFSET,
     width: width + (PADDING * 2) - PADDING_OFFSET,
     height: height + (PADDING * 2) - PADDING_OFFSET
   };
-
-  if (rotated) {
-    const o = { x, y };
-    const labelBounds = {
-      x, y, width, height
-    };
-    const rotatedBounds = pointsToRect(rectToPoints(labelBounds).map(p => rotate(p, toRadians(-90), o)));
-    bounds = {
-      x: rotatedBounds.x + height * (1 - delta) - PADDING - PADDING_OFFSET, // bounds is based on text-before-edge and then flipped to the left so it needs to shift rigth about 25% (100% - 75%) to match the label is based on alphabetical
-      y: rotatedBounds.y - PADDING - PADDING_OFFSET,
-      width: rotatedBounds.width + (PADDING * 2) - PADDING_OFFSET,
-      height: rotatedBounds.height + (PADDING * 2) - PADDING_OFFSET
-    };
-  }
   return bounds;
 }
 
