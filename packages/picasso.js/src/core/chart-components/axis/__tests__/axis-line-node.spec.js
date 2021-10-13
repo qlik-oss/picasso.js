597ea4ecfc72c51de386b4e5ba9d3257d291d65a
Christian Veinfors <cvs@qlik.com>
2021-10-13 15:20:41 +0200

    fix: consider offset when generating axis lines (#636)

--- packages/picasso.js/src/core/chart-components/axis/__tests__/axis-line-node.spec.js
+++ packages/picasso.js/src/core/chart-components/axis/__tests__/axis-line-node.spec.js
@@ -14,21 +14,18 @@ describe('Axis Line Node', () => {
     height: 0,
   };
 
-  beforeEach(() => {
-    innerRect.width = 50;
-    innerRect.height = 100;
-    innerRect.x = 0;
-    innerRect.y = 0;
-    outerRect.width = 50;
-    outerRect.height = 100;
-    outerRect.x = 0;
-    outerRect.y = 0;
-  });
-
   describe('Line', () => {
     let buildOpts, expected;
 
     beforeEach(() => {
+      innerRect.width = 50;
+      innerRect.height = 100;
+      innerRect.x = 0;
+      innerRect.y = 0;
+      outerRect.width = 50;
+      outerRect.height = 100;
+      outerRect.x = 0;
+      outerRect.y = 0;
       buildOpts = {
         style: { stroke: 'red', strokeWidth: 1 },
         align: 'bottom',
@@ -80,6 +77,52 @@ describe('Axis Line Node', () => {
       expected.y1 = buildOpts.padding;
       expected.y2 = buildOpts.padding;
       expect(buildLine(buildOpts)).to.deep.equal(expected);
+    });
+
+    describe('Within an offset container', () => {
+      it('Left align', () => {
+        buildOpts.align = 'left';
+        innerRect.y = 125;
+        outerRect.y = 100;
+        expected.x1 = innerRect.width - buildOpts.padding;
+        expected.x2 = innerRect.width - buildOpts.padding;
+        expected.y1 = 25;
+        expected.y2 = 125;
+        expect(buildLine(buildOpts)).to.deep.equal(expected);
+      });
+
+      it('Right align', () => {
+        buildOpts.align = 'right';
+        innerRect.y = 125;
+        outerRect.y = 100;
+        expected.x1 = buildOpts.padding;
+        expected.x2 = buildOpts.padding;
+        expected.y1 = 25;
+        expected.y2 = 125;
+        expect(buildLine(buildOpts)).to.deep.equal(expected);
+      });
+
+      it('Top align', () => {
+        buildOpts.align = 'top';
+        innerRect.x = 125;
+        outerRect.x = 100;
+        expected.x1 = 25;
+        expected.x2 = 75;
+        expected.y1 = innerRect.height - buildOpts.padding;
+        expected.y2 = innerRect.height - buildOpts.padding;
+        expect(buildLine(buildOpts)).to.deep.equal(expected);
+      });
+
+      it('Bottom align', () => {
+        buildOpts.align = 'bottom';
+        innerRect.x = 125;
+        outerRect.x = 100;
+        expected.x1 = 25;
+        expected.x2 = 75;
+        expected.y1 = buildOpts.padding;
+        expected.y2 = buildOpts.padding;
+        expect(buildLine(buildOpts)).to.deep.equal(expected);
+      });
     });
   });
 });
