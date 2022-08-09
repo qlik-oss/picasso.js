75a6f68a59740f43f98fb72b87a8bf0abcdfd63f
Jingjing <8jjxie@gmail.com>
2022-08-09 08:43:30 +0200

    fix: use qElemNumber instead

--- plugins/q/src/data/extractor-t.js
+++ plugins/q/src/data/extractor-t.js
@@ -267,7 +267,7 @@ function getHierarchyForSMode(dataset) {
     let isNew = false;
     for (let c = 0; c < dimensions.length; c++) {
       const cell = row[dimensions[c]];
-      const key = `${id}__${cell.qText}__${cell.qNum}`;
+      const key = `${id}__${cell.qText}__${cell.qElemNumber}`;
       if (!keys[key]) {
         keys[key] = {
           __id: key,
