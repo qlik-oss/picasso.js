d678cf58eb4800edd37ddeb5f36ac69ec0ebdc61
Niclas Fors <niclas.fors@gmail.com>
2022-09-27 12:36:41 +0200

    fix(axis): measure text correctly when checking overlapping labels (#708)

--- packages/picasso.js/src/core/chart-components/axis/axis-label-size.js
+++ packages/picasso.js/src/core/chart-components/axis/axis-label-size.js
@@ -222,7 +222,7 @@ export default function getSize({ isDiscrete, rect, formatter, measureText, scal
     if (state.labels.activeMode === 'tilted') {
       const extendLeft = (settings.align === 'bottom') === settings.labels.tiltAngle >= 0;
       const radians = Math.abs(settings.labels.tiltAngle) * (Math.PI / 180); // angle in radians
-      const h = measureText('M').height;
+      const h = measure('M').height;
       const maxWidth = (textSize - h * Math.cos(radians)) / Math.sin(radians);
       const labelWidth = (r) => Math.min(maxWidth, r.width) * Math.cos(radians) + r.height;
       const adjustByPosition = (s, i) => {
@@ -242,7 +242,7 @@ export default function getSize({ isDiscrete, rect, formatter, measureText, scal
         !settings.labels.filterOverlapping &&
         isTiltedLabelOverlapping({
           majorTicks,
-          measureText,
+          measureText: measure,
           rect,
           bleedSize,
           angle: settings.labels.tiltAngle,
