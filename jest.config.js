595bf2c6b52168b83347c1e6fdc91cfda910508e
Niclas Fors <niclas.fors@qlik.com>
2022-10-03 19:48:22 +0200

    chore: remove transform from jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/src/**/__tests__/*.spec.js'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  transform: {
-    '\\.[jt]sx?$': 'babel-jest',
-  },
   transformIgnorePatterns: ['node_modules/(?!preact/.*)'],
   coverageReporters: ['text-summary', 'lcov'],
   coverageDirectory: './coverage',
