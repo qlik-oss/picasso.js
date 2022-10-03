b2818c392feb78856081229ef74732d4fab186f3
Niclas Fors <niclas.fors@qlik.com>
2022-10-03 20:04:20 +0200

    chore: put transform back in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/src/**/__tests__/*.spec.js'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+  },
   transformIgnorePatterns: ['node_modules/(?!preact/.*)'],
   coverageReporters: ['text-summary', 'lcov'],
   coverageDirectory: './coverage',
