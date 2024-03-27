45a9213de46eb7bd28e617cb3d9ed6cd7e70745f
Christofer Bengtsson <cbt@qlik.com>
2024-03-27 12:21:45 +0100

    chore: remove studio

--- studio/src/components/index.js
+++
@@ -1,5 +0,0 @@
-import CodeArea from './CodeArea/CodeArea';
-import ExampleList from './ExampleList/ExampleList';
-import RenderingArea from './RenderingArea/RenderingArea';
-
-export { CodeArea, ExampleList, RenderingArea };
--- studio/src/core/sense/mixins/index.js
+++
@@ -1,6 +0,0 @@
-import base from './base';
-import { mixin } from './sheets';
-import layoutObservable from './layout-observable';
-import object from './object';
-
-export default [base, mixin, layoutObservable, object];
