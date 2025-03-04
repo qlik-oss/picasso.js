9c5187401b1ac84f792cfa4cefeb758b4abc403f
Axel Nilsson <rtb@qlik.com>
2025-03-04 15:29:16 +0100

    refactor: renamed constants to correct names

--- packages/picasso.js/src/web/text-manipulation/text-ellipsis.js
+++ packages/picasso.js/src/web/text-manipulation/text-ellipsis.js
@@ -2,8 +2,8 @@ import { ELLIPSIS_CHAR } from './text-const';
 
 export default function ellipsText(label, measureText) {
   // eslint-disable-line import/prefer-default-export
-  const fontSize = label['font-family'] || label.fontFamily;
-  const fontFamily = label['font-size'] || label.fontSize;
+  const fontFamily = label['font-family'] || label.fontFamily;
+  const fontSize = label['font-size'] || label.fontSize;
   const text = typeof label.text === 'string' ? label.text : `${label.text}`;
   const { maxWidth } = label;
   if (maxWidth === undefined) {
