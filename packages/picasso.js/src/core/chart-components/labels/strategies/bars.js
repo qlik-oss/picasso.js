96414c8c5b81a2b37313c08aad6bf462cac70b46
Bao Ho <bao.hodang@gmail.com>
2025-04-24 11:00:45 +0200

    fix: bar label strategy dock right

--- packages/picasso.js/src/core/chart-components/labels/strategies/bars.js
+++ packages/picasso.js/src/core/chart-components/labels/strategies/bars.js
@@ -89,7 +89,11 @@ export function placeTextInRect(rect, text, opts) {
   if (opts.rotate) {
     label.x = placeSegmentInSegment(rect.x, rect.width, textMetrics.height, opts.align) + baseLineOffset;
     label.y = placeSegmentInSegment(rect.y, rect.height, textMetrics.width, opts.justify);
-    label.transform = `rotate(-90, ${label.x + label.dx}, ${label.y + label.dy})`;
+    if (opts.dock === 'right') {
+      label.transform = `rotate(90, ${label.x + label.dx}, ${label.y + label.dy}) translate(${textMetrics.width}, 0)`;
+    } else {
+      label.transform = `rotate(-90, ${label.x + label.dx}, ${label.y + label.dy})`;
+    }
   } else {
     label.x = placeSegmentInSegment(rect.x, rect.width, textMetrics.width, opts.align);
     label.y = placeSegmentInSegment(rect.y, rect.height, textMetrics.height, opts.justify) + baseLineOffset;
@@ -314,6 +318,7 @@ export function placeInBars(
           textMetrics: measured,
           rotate: isRotated,
           overflow: !!overflow,
+          dock: lblStngs.dock,
         });
 
         if (label) {
